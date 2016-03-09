b3559c38e5f173e254b88d7f1ecd2597f066bf9c
perlahj <perlahj@gmail.com>
2016-03-09 12:04:41 +0100

    Testing github
    added one random test to seller test

--- Assignment3/src/components/sellers/SellersController.spec.js
+++ Assignment3/src/components/sellers/SellersController.spec.js
@@ -1,5 +1,15 @@
 "use strict";
 
+
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+
+
 describe("SellersController should be unit tested here", function() {
 	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
 });
