3bb199b44e5652845b11ee356ed8e4ad2a6672cc
audur14 <audurelisa@Audur-Elisa-MacBook-Pro.local>
2016-03-10 14:36:00 +0100

    validation virkar

--- Assignment3/.build/scripts/app-e358a7eb.js
+++ Assignment3/.build/scripts/app-e358a7eb.js
@@ -292,6 +292,7 @@ function SellerDlg($uibModal) {
 "use strict";
 
 angular.module("project3App").controller("SellerDlgController",
+	["$scope",
 function SellerDlgController($scope) {
 	
 	$scope.seller = {
@@ -302,21 +303,25 @@ function SellerDlgController($scope) {
 
 	};
 
-	$scope.onOk = function onOk() {
-		//TODO: validation - þannig að það verði ekki lokað glugganum nema validatist!!
-		
-		if ($scope.seller.name.length === 0 || $scope.seller.category.length === 0) {
+	$scope.onSubmit = function() {
+		//if ($scope.seller.name.length === 0 || $scope.seller.category.length === 0) {
+		/*if ($scope.addForm.$invalid) { //á að vera valid
+
 			//Birta validation skilaboð
 			return;
 		}
-		$scope.$close($scope.seller);
+		$scope.$close($scope.seller);*/
+
+		if ($scope.addForm.$valid) { //á að vera valid
+			$scope.$close($scope.seller);
+		}
 	};
 
-	$scope.onCancel = function onCancel() {
+	$scope.onCancel = function() {
 		$scope.$dismiss();
 	}; 
 
-});
+}]);
 "use strict";
 
 angular.module("project3App").controller("SellersController",
--- Assignment3/src/components/seller-dlg/SellerDlgController.js
+++ Assignment3/src/components/seller-dlg/SellerDlgController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 angular.module("project3App").controller("SellerDlgController",
+	["$scope",
 function SellerDlgController($scope) {
 	
 	$scope.seller = {
@@ -11,18 +12,22 @@ function SellerDlgController($scope) {
 
 	};
 
-	$scope.onOk = function onOk() {
-		//TODO: validation - þannig að það verði ekki lokað glugganum nema validatist!!
-		
-		if ($scope.seller.name.length === 0 || $scope.seller.category.length === 0) {
+	$scope.onSubmit = function() {
+		//if ($scope.seller.name.length === 0 || $scope.seller.category.length === 0) {
+		/*if ($scope.addForm.$invalid) { //á að vera valid
+
 			//Birta validation skilaboð
 			return;
 		}
-		$scope.$close($scope.seller);
+		$scope.$close($scope.seller);*/
+
+		if ($scope.addForm.$valid) { //á að vera valid
+			$scope.$close($scope.seller);
+		}
 	};
 
-	$scope.onCancel = function onCancel() {
+	$scope.onCancel = function() {
 		$scope.$dismiss();
 	}; 
 
-});
+}]);
