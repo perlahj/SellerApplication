9bb6ae168f539a1ee058a6b1b4c015a5b3917273
audur14 <audurelisa@Audur-Elisa-MacBook-Pro.local>
2016-03-10 21:57:24 +0100

    validate edit and validate image in both add and edit

--- Assignment3/src/components/seller-dlgEdit/SellerDlgEditController.js
+++ Assignment3/src/components/seller-dlgEdit/SellerDlgEditController.js
@@ -13,10 +13,30 @@ function SellerDlgEditController($scope, $routeParams, AppResource) {
 			$scope.isLoading = false;
 		});
 
-	$scope.onOk = function onOk() {
-		//TODO: validation - þannig að það verði ekki lokað glugganum nema validatist!!
-		
-		if ($scope.seller.name.length === 0 || $scope.seller.category.length === 0) {
+	$scope.onSubmit = function onSubmit() {
+		if ($scope.editForm.$valid) {	
+			var newSeller = {
+				name : $scope.seller.name,
+				category : $scope.seller.category,
+				imagePath : $scope.seller.imagePath
+			};
+			console.log(newSeller);
+
+			AppResource.updateSeller(sellerId, newSeller).success(function(returnedSeller) {
+					console.log("Updated seller successfully");
+					console.log(returnedSeller);
+					$scope.seller = returnedSeller;
+				}).error(function() {
+					//centrisNotify.error("sellers.Messages.SaveFailed");
+					console.log("Error updating seller");
+				});
+			$scope.$close($scope.seller);
+		}
+	};
+
+		/*//TODO: validation - þannig að það verði ekki lokað glugganum nema validatist!!
+		if ($scope.editForm.$invalid) {	
+			//	if ($scope.seller.name.length === 0 || $scope.seller.category.length === 0) {
 			//Birta validation skilaboð
 			return;
 		}
@@ -36,7 +56,7 @@ function SellerDlgEditController($scope, $routeParams, AppResource) {
 				console.log("Error updating seller");
 			});
 		$scope.$close($scope.seller);
-	};
+	};*/
 
 	$scope.onCancel = function onCancel() {
 		$scope.$dismiss();
