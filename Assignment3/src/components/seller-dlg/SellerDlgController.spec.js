e8ad000a9e55c470a032eb4d1b781d6941ad4767
perlahj <perlahj@gmail.com>
2016-03-11 19:57:23 +0100

    test

---
+++ Assignment3/src/components/seller-dlg/SellerDlgController.spec.js
@@ -0,0 +1,31 @@
+"use strict";
+
+describe("SellerDlgController", function() {
+	beforeEach(module("project3App"));
+	var scope, ctrl;
+
+	var mockFormValid = true;
+
+	describe('', function() {
+		beforeEach(module('pascalprecht.translate', function($translateProvider) {
+			$translateProvider.translations('is', {
+				"seller": "seljandi"
+			});
+			$translateProvider.translations('es', {
+				"seller": "seller"
+			});
+			$translateProvider.preferredLanguage('is');
+		}));
+
+		beforeEach(inject(function($controller, $rootScope, $translate) {
+			scope = $rootScope.$new();
+			ctrl = $controller("SellerDlgController", {
+				$scope: scope
+			});
+		}));
+
+		it("should return true", function(){
+			expect(mockFormValid).toBe(true);
+		})
+	});
+});
